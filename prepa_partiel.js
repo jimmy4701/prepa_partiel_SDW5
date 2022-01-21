c6119fe256402faeb45beb7eea9997614568d935
Jérémy SIMONKLEIN ANGELY <jeremy.simonklein@gmail.com>
2022-01-21 12:22:55 +0100

    Affichage du compteur de clics dans le HTML

--- prepa_partiel.js
+++ prepa_partiel.js
@@ -13,13 +13,20 @@ var total_clicks = 0
 // Récupérer le bouton qui a l'id "btn"
 const bouton = document.getElementById('btn')
 
+// Récupérer une balise p avec l'id "counter"
+const counter = document.getElementById('counter')
+
+
 // Fonction qui gère ce que l'on fait à chaque clic
 
 // Quand on appelle handleClick, ça doit ajouter 1 à la variable "total_clicks"
 // et afficher la variable total_clicks en console
+// et modifier le .innerText de la balise qui a l'id counter avec le total de clics
+// 
 const handleClick = () => {
     total_clicks++
     console.log("Total de clics", total_clicks)
+    counter.innerText = total_clicks
 }
 
 // Ajout d'un event listener sur le bouton pour gérer les clics
