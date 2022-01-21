f9fb668668b8faf87d220104582f73b014fda90d
Jérémy SIMONKLEIN ANGELY <jeremy.simonklein@gmail.com>
2022-01-21 11:43:30 +0100

    Fonction 'addition'

--- prepa_partiel.js
+++ prepa_partiel.js
@@ -13,5 +13,19 @@ var total_clicks = 0
 // Récupérer le bouton qui a l'id "btn"
 const bouton = document.getElementById('btn')
 
+// Fonction qui gère ce que l'on fait à chaque clic
+const handleClick = (value) => {
+    console.log("La valeur est ", value)
+}
+
+// Créez une fonction "addition" qui prend deux paramètres a et b et qui écrit
+// la somme de a et b en console
+// Ex: si j'appelle addition(20, 13) la console doit afficher 33
+const addition = (a,b) => {
+    console.log(a + b)
+}
+
+addition(20 , 13)
+
 console.log("Total de clics", total_clicks)
 
